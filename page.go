13658ed498bd27f08cd0540a2dc0c7aa373f90e7
H_C_Q <hechuangqiang@gmail.com>
2015-12-06 16:09:04 +0800

    add page.go

---
+++ page.go
@@ -0,0 +1,39 @@
+package orm
+
+type Page struct {
+	PageNo      int
+	PageSize    int
+	ResultCount int
+	List        interface{}
+}
+
+func NewPage(pageNo, pageSize int) *Page {
+	p := Page{}
+	if pageNo == 0 {
+		p.PageNo = 1
+	} else {
+		p.PageNo = pageNo
+	}
+
+	if pageSize == 0 {
+		p.PageSize = 15
+	} else {
+		p.PageSize = pageSize
+	}
+
+	return &p
+}
+
+func (p *Page) StartRow() int {
+	return p.PageSize*(p.PageNo-1) + 1
+}
+
+func (p *Page) PageCount() int {
+	pageCount := 0
+	if p.ResultCount%p.PageSize == 0 {
+		pageCount = p.ResultCount / p.PageSize
+	} else {
+		pageCount = p.ResultCount/p.PageSize + 1
+	}
+	return pageCount
+}
